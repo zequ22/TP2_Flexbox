c399d90cda6416a876d1708ac441ef49f2bb32a2
zequ22 <eze22alderete@gmail.com>
2023-06-30 16:57:22 -0300

    Correcciones en CSS y Validaciones y Restructuracion del Registro

--- validaciones.js
+++ validaciones.js
@@ -1,69 +1,175 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('.formu');
-    const nombreC = document.getElementById('nombreC');
-    const correoE = document.getElementById('correoE');
-    const pass1 = document.getElementById('pass1');
-    const pass2 = document.getElementById('pass2');
-    const edad = document.getElementById('edad');
-    const tel = document.getElementById('tel');
-    const direcc = document.getElementById('direcc');
-    const ciudad = document.getElementById('ciudad');
-    const codpost = document.getElementById('codpost');
-    const DNI = document.getElementById('DNI');
-  
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-  
-      if (nombreC.value.length < 6 || !nombreC.value.includes(' ')) {
-        alert('El nombre completo debe tener más de 6 letras y al menos un espacio entre medio.');
-        return;
-      }
-      if (!validateEmail(correoE.value)) {
-        alert('El email debe tener un formato válido.');
-        return;
-      }  
-      if (pass1.value.length < 8 || !pass1.value.match(/^[a-zA-Z0-9]+$/)) {
-        alert('La contraseña debe tener al menos 8 caracteres, formados por letras y números.');
-        return;
-      }  
-      if (pass1.value !== pass2.value) {
-        alert('Las contraseñas no coinciden.');
-        return;
-      }  
-      if (parseInt(edad.value) < 18 || !Number.isInteger(parseFloat(edad.value))) {
-        alert('La edad debe ser un número entero mayor o igual a 18.');
-        return;
-      }  
-      if (!tel.value.match(/^\d{7,}$/)) {
-        alert('El teléfono debe tener al menos 7 dígitos y no debe contener espacios, guiones ni paréntesis.');
-        return;
-      }  
-      if (direcc.value.length < 5 || !direcc.value.match(/^[a-zA-Z0-9]+ [a-zA-Z0-9]+$/)) {
-        alert('La dirección debe tener al menos 5 caracteres, con letras, números y un espacio en el medio.');
-        return;
-      }  
-      if (ciudad.value.length < 3) {
-        alert('La ciudad debe tener al menos 3 caracteres.');
-        return;
-      }  
-      if (codpost.value.length < 3) {
-        alert('El código postal debe tener al menos 3 caracteres.');
-        return;
-      }  
-      if (!DNI.value.match(/^\d{7,8}$/)) {
-        alert('El DNI debe ser un número de 7 u 8 dígitos.');
-        return;
-      }
-  
-      // Si todas las validaciones pasan, puedes enviar el formulario
-      form.submit()
-      alert('Registro exitoso.');
-    });
-  
-    // Función para validar el formato del email
-    function validateEmail(email) {
-      const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return re.test(email);
-    }
-  });
-  
+//Selectores
+var nombre = document.getElementById('nombreC');
+var nombreAlert = document.getElementById('nombreAlert');
+var email = document.getElementById('correoE');
+var emailAlert = document.getElementById('emailAlert');
+var contra = document.getElementById('pass1');
+var contraAlert = document.getElementById('contraAlert');
+var recontra = document.getElementById('pass2');
+var recontraAlert = document.getElementById('recontraAlert');
+var edad = document.getElementById('edad');
+var edadAlert = document.getElementById('edadAlert');
+var tel = document.getElementById('tel');
+var telAlert = document.getElementById('telAlert');
+var dir = document.getElementById('direcc');
+var dirAlert = document.getElementById('dirAlert');
+var ciudad = document.getElementById('ciudad');
+var ciudadAlert = document.getElementById('ciudadAlert');
+var codPost = document.getElementById('codpost');
+var codPostAlert = document.getElementById('codPostAlert');
+var dni = document.getElementById('DNI');
+var dniAlert = document.getElementById('dniAlert');
+var hola = document.getElementById('hi');
+var btnEnviar = document.getElementById('btnEnviar').addEventListener('click', enviar);
+
+// RegEx
+var alfNumRegEx = /^[a-zA-Z0-9]+$/;
+var alNuSpRegEx = /^[a-zA-Z0-9_ ]*$/;
+var numRegEx = /^[0-9]+$/;
+var alfRegEx = /^[a-zA-Z]+$/;
+var mailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Funciones
+function enviar()
+{
+	alert('¡Datos enviados correctamente!')
+}
+
+function hide(id)
+{
+	var next = id.currentTarget.nextElementSibling;
+	if (next.style.visibility !== 'hidden')
+	{
+		next.style.visibility = 'hidden';
+	}
+}
+
+function show(id)
+{
+	id.style.visibility = 'visible';
+}
+
+nombre.addEventListener('blur', checkNombre);
+nombre.addEventListener('focus', hide);
+
+function checkNombre()
+{
+	if (!(nombre.value.length > 6 && nombre.value.includes(' ')))
+	{
+		show(nombreAlert);
+	}
+}
+
+nombre.addEventListener('input', updateHi);
+
+function updateHi()
+{
+	if (nombre.value === '')
+	{
+		hola.textContent = '¡Hola!'
+	}
+	else
+	{
+		hola.textContent = '¡Hola ' + nombre.value + '!';
+	}	
+}
+
+email.addEventListener('blur', checkEmail);
+email.addEventListener('focus', hide);
+
+function checkEmail()
+{
+	if (!(mailRegEx.test(email.value)))
+	{
+		show(emailAlert);
+	}
+}
+
+contra.addEventListener('blur', checkContra);
+contra.addEventListener('focus', hide);
+
+function checkContra()
+{
+	if (!(contra.value.length >= 8 && alfNumRegEx.test(contra.value)))
+	{
+		show(contraAlert);
+	}
+}
+
+recontra.addEventListener('blur', checkRecontra);
+recontra.addEventListener('focus', hide);
+
+function checkRecontra()
+{
+	if (recontra.value !== contra.value)
+	{
+		show(recontraAlert);
+	}
+}
+
+edad.addEventListener('blur', checkEdad);
+edad.addEventListener('focus', hide);
+
+function checkEdad()
+{
+	if(edad.value < 18)
+	{
+		show(edadAlert);
+	}
+}
+
+tel.addEventListener('blur', checkTel);
+tel.addEventListener('focus', hide);
+
+function checkTel()
+{
+	if (!(tel.value.length >= 7 && numRegEx.test(tel.value)))
+	{
+		show(telAlert);
+	}
+}
+
+dir.addEventListener('blur', checkDir);
+dir.addEventListener('focus', hide);
+
+function checkDir()
+{
+	if (!(dir.value.length >= 5 && alNuSpRegEx.test(dir.value)))
+	{
+		show(dirAlert);
+	}
+}
+
+ciudad.addEventListener('blur', checkCiudad);
+ciudad.addEventListener('focus', hide);
+
+function checkCiudad()
+{
+	if (ciudad.value.length < 3)
+	{
+		show(ciudadAlert);
+	}
+}
+
+
+codPost.addEventListener('blur', checkCodPost);
+codPost.addEventListener('focus', hide);
+
+function checkCodPost()
+{
+	if (codPost.value.length < 3)
+	{
+		show(codPostAlert);
+	}
+}
+
+dni.addEventListener('blur', checkDNI);
+dni.addEventListener('focus', hide);
+
+function checkDNI()
+{
+	if (!(dni.value.length >= 5 && numRegEx.test(dni.value)))
+	{
+		show(dniAlert);
+	}
+}
